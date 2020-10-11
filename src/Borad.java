eb6a6c063fb9298a3b3920f340b99d0a86dada68
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2020-10-12 00:08:22 +0700

    วางตำแหน่งของนักวิ่ง

--- src/Borad.java
+++ src/Borad.java
@@ -1,11 +1,10 @@
 
 import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.awt.Toolkit;
 import javax.swing.JPanel;
 
 public class Borad extends JPanel {
@@ -18,37 +17,54 @@ public class Borad extends JPanel {
 	Timer t = new Timer();
 	private int size_x = s.getX();
 	private int size_y = s.getY();
-	
+
 	int run_x = 0;
 	int run_y = 0;
+	int X = 0;
+	int Y = 0;
 	boolean back = false;
 
 	public Borad() {
 		setSize(size_x, size_y);
 		setLocation(0, 0);
 		setLayout(null);
-		t.scheduleAtFixedRate(new Move(this), 0, new Random().nextInt(30)+5);
-		repaint();
-	}
+		t.scheduleAtFixedRate(new Move(this), 0, new Random().nextInt(30) + 5);
 
-	public Image getImage(String Url) {
-		return Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + "src"
-				+ File.separator + "images" + File.separator + Url);
+		this.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				X = e.getX();
+				Y = e.getY();
+			}
+
+		});
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.drawImage(p.background, 0, 0, size_x-15, size_y-38, this);
-		if (run_x == 900 || back) {
-			back = true;
-			g.drawImage(p.p1_right, run_x, run_y, 100, 100, this);
-			if (run_x == 0) {
-				back = false;
-			}
-		} else {
-			g.drawImage(p.p1_left, run_x, run_y, 100, 100, this);
-		}
+		g.drawImage(p.background, 0, 0, size_x - 15, size_y - 38, this);
+		g.drawImage(p.p1_right, 565, 485, 80, 80, this);
+		g.drawImage(p.p1_right, 610, 525, 80, 80, this);
+		g.drawImage(p.p1_right, 660, 560, 80, 80, this);
 
+		g.drawImage(p.p1_left, 990, 235, 80, 80, this);
+		g.drawImage(p.p1_left, 1040, 270, 80, 80, this);
+		g.drawImage(p.p1_left, 1090, 305, 80, 80, this);
+
+		g.drawImage(p.p1_left, 555, 0, 80, 80, this);
+		g.drawImage(p.p1_left, 515, 25, 80, 80, this);
+		g.drawImage(p.p1_left, 465, 55, 80, 80, this);
+
+		g.drawImage(p.p1_right, 15, 235, 80, 80, this);
+		g.drawImage(p.p1_right, 65, 270, 80, 80, this);
+		g.drawImage(p.p1_right, 115, 305, 80, 80, this);
 	}
 }
 
@@ -62,7 +78,7 @@ class Move extends TimerTask {
 	@Override
 	public void run() {
 		if (panel.back) {
-			panel.run_y +=1;
+			panel.run_y += 1;
 			if (panel.run_y == 500) {
 				panel.back = false;
 			}
@@ -74,6 +90,6 @@ class Move extends TimerTask {
 		}
 
 		this.panel.repaint();
-		System.out.print("\r" + panel.run_x + " " + "0");
+		System.out.print("\r" + panel.X + " " + panel.Y);
 	}
 }
