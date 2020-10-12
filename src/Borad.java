c64f99ed785fd89357a23a7ef4ff16877dbfdc06
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2020-10-12 21:09:21 +0700

    ใกล้ได้ละ

--- src/Borad.java
+++ src/Borad.java
@@ -2,10 +2,11 @@
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.Image;
+import javax.swing.JLabel;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -16,84 +17,84 @@ public class Borad extends JPanel {
 	private static final long serialVersionUID = 1L;
 	GameFrame s = new GameFrame();
 	Picture p = new Picture();
-	Timer t = new Timer();
+
 	JButton newButton = new JButton("New Game");
 	JButton ssButton = new JButton("Start");
 	private int size_x = s.getX();
 	private int size_y = s.getY();
 
-	int run_x = 0;
-	int run_y = 0;
-	int X = 0;
-	int Y = 0;
-	boolean back = false;
+	JLabel[] Character = new JLabel[12];
+	int[] X = { 565, 610, 660, 990, 1040, 1090, 555, 515, 465, 15, 65, 115 };
+	int[] Y = { 485, 525, 560, 235, 270, 305, 0, 25, 55, 235, 270, 305 };
+
+	int[] X1 = { 565, 610, 660, 990, 1040, 1090, 555, 515, 465, 15, 65, 115 };
+	int[] Y1 = { 485, 525, 560, 235, 270, 305, 0, 25, 55, 235, 270, 305 };
 
 	public Borad() {
 		setSize(size_x, size_y);
 		setLocation(0, 0);
 		setLayout(null);
-		t.scheduleAtFixedRate(new Move(this), 0, new Random().nextInt(30) + 5);
 
-		this.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				X = e.getX();
-				Y = e.getY();
-			}
-		});
-		newButton.setSize(200,50);
-		newButton.setLocation(190,150);
+		newButton.setSize(200, 50);
+		newButton.setLocation(190, 150);
 
-		ssButton.setSize(200,50);
-		ssButton.setLocation(190,210);
+		ssButton.setSize(200, 50);
+		ssButton.setLocation(190, 210);
 		add(newButton);
 		add(ssButton);
+
+		for (int i = 0; i < 12; i++) {
+			Character[i] = new JLabel(new ImageIcon(p.p1_right.getScaledInstance(80, 80, Image.SCALE_DEFAULT)));
+			Character[i].setLocation(X[i], Y[i]);
+			Character[i].setSize(80, 80);
+			add(Character[i]);
+		}
+
+		Move c1 = new Move(Character[0],0);
+		c1.setSleep();
+		Move c2 = new Move(Character[1],1);
+		c2.setSleep();
+		// Move c3 = new Move(Character[2], 2, 5, stop, go, 1);
+
+		c1.start();
+		c2.start();
+		// c3.start();
+
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.drawImage(p.background, 0, 0, size_x - 15, size_y - 38, this);
-		//ชุดแรก
-		g.drawImage(p.p1_right, 565, 485, 80, 80, this);
-		g.drawImage(p.p1_right, 610, 525, 80, 80, this);
-		g.drawImage(p.p1_right, 660, 560, 80, 80, this);
-		//ชุด2
-		g.drawImage(p.p1_left, 990, 235, 80, 80, this);
-		g.drawImage(p.p1_left, 1040, 270, 80, 80, this);
-		g.drawImage(p.p1_left, 1090, 305, 80, 80, this);
-		//ชุด3
-		g.drawImage(p.p1_left, 555, 0, 80, 80, this);
-		g.drawImage(p.p1_left, 515, 25, 80, 80, this);
-		g.drawImage(p.p1_left, 465, 55, 80, 80, this);
-		//ชุด4
-		g.drawImage(p.p1_right, 15, 235, 80, 80, this);
-		g.drawImage(p.p1_right, 65, 270, 80, 80, this);
-		g.drawImage(p.p1_right, 115, 305, 80, 80, this);
-	}
-}
-
-class Move extends TimerTask {
-	private Borad panel;
-
-	Move(Borad panel) {
-		this.panel = panel;
 	}
 
-	@Override
-	public void run() {
-		if (panel.back) {
-			panel.run_y += 1;
-			if (panel.run_y == 500) {
-				panel.back = false;
-			}
-		} else {
-			if (panel.run_y == 0 || panel.back) {
-				panel.back = true;
-			}
-			panel.run_y += 1;
+	class Move extends Thread {
+		private JLabel label;
+		private int index;
+		private int sleep = 0;
+
+		Move(JLabel label,int index) {
+			this.label = label;
+			this.index = index;
 		}
-
-		this.panel.repaint();
-		System.out.print("\r" + panel.X + " " + panel.Y);
+	
+		public void setSleep() {
+			this.sleep = new Random().nextInt(30)+1;
+		}
+	
+		@Override
+		public void run() {
+			while (true) {
+				label.setLocation(X[index]++,Y[index]);
+				try {
+					Thread.sleep(sleep);
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}
+	
+		}
 	}
 }
+
+
+
